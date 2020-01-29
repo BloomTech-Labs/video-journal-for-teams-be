9c34186ea3c45ae7f41b5fbacbe9bf9f1c460c19
Michael Nunes <tinysquid.git@gmail.com>
2020-01-28 16:49:32 -0800

    Add routes for logging in with email/username

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,13 +1,33 @@
-const express = require('express');
-
-const Users = require('../users/userModel.js');
-
+const express = require("express");
+const Users = require("../users/userModel.js");
+const passport = require("passport");
 const router = express.Router();
+const { signToken } = require("../middleware/middleware");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Users.find()
-  .then(users => res.status(200).json(users))
-  .catch(err => console.log(err))
-})
+    .then((users) => res.status(200).json(users))
+    .catch((err) => console.log(err));
+});
+
+router.post("/login/email", passport.authenticate("email-login", { session: false }), function(req, res) {
+  res.status(200).json(loginSuccessBody(req.user));
+});
+
+router.post("/login/username", passport.authenticate("username-login", { session: false }), function(req, res) {
+  res.status(200).json(loginSuccessBody(req.user));
+});
 
 module.exports = router;
+
+function loginSuccessBody(user) {
+  const token = signToken({ sub: user.id });
+
+  return {
+    user: {
+      email: user.email,
+      username: user.username,
+    },
+    token: token,
+  };
+}
