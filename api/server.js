bdf7bba238d39ce85447a27ccfecf0321d4bb331
Michael Nunes <tinysquid.git@gmail.com>
2020-01-28 16:48:46 -0800

    Add passport to server

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const UserRouter = require('../users/userRouter.js');
-const TeamRouter = require('../teams/teamRouter.js');
+const passport = require("passport");
+require("../passport/index");
+
+const UserRouter = require("../users/userRouter.js");
+const TeamRouter = require("../teams/teamRouter.js");
 
 const server = express();
 
@@ -11,11 +14,13 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/users', UserRouter);
-server.use('/api/teams', TeamRouter);
+server.use(passport.initialize());
 
-server.get('/', (req, res) => {
-  res.status(200).json({ api: 'running' });
+server.use("/api/users", UserRouter);
+server.use("/api/teams", TeamRouter);
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "running" });
 });
 
 module.exports = server;
