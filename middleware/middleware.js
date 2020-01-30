f31834d937364c473a98aeba054604b1d1f62c7c
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2020-01-30 09:40:10 -0800

    Resolved another merge conflict and moved validateUserId function onto its own file.

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,29 +1,9 @@
 const signToken = require("./signToken");
-<<<<<<< HEAD
-const Users = require("../users/userModel.js");
-
-module.exports = {
-  signToken,
-  validateUserId
-=======
 const validateSignup = require("./validateSignup");
+const validateUserId = require("./validateUserId");
 
 module.exports = {
   signToken,
-  validateSignup,
->>>>>>> dbb875b32fea1e34136f946c7cca86576219a0cd
+  validateUserId,
+  validateSignup
 };
-
-function validateUserId(req, res, next) {
-  const { id } = req.params
-
-  Users.findById(id)
-    .then((user) => {
-      if (user) {
-        next()
-      } else {
-        res.status(400).json({ message: "Invalid user id."})
-      }
-    })
-    .catch(err => res.status(500).json({ message: "'Failed to get user from database'", error: err }))
-}
