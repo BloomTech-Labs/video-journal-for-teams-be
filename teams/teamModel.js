e265abc41ec11f1e6d5f19c7187fce455c62aff2
Kerri-Ann <kaybee1jam@yahoo.com>
2020-02-03 16:30:01 -0500

    added endpoints for teams. PUT, POST, and GET to get all users for a specified team

--- teams/teamModel.js
+++ teams/teamModel.js
@@ -3,7 +3,10 @@ const db = require("../database/dbConfig.js");
 module.exports = {
   find,
   findById,
-  findByUserId
+  findByUserId,
+  insertUser,
+  update,
+  getUsersByTeamId
 };
 
 function find() {
@@ -24,4 +27,26 @@ function findByUserId(user_id) {
     .join("team_members", "teams.id", "team_members.team_id")
     .where("team_members.user_id", user_id)
     .select("teams.id as id", "teams.name as name", "teams.description as description", "teams.created_at as created_at", "teams.updated_at as updated_at", "team_members.role_id as role_id")
+}
+
+// Inset user to a team
+function insertUser(data) {
+  return db("team_members")
+    .insert(data);
+}
+
+// Update team info
+function update(id, changes) {
+  return db("teams")
+    .where({ id })
+    .update(changes)
+}
+
+// Get users in a spcified team
+function getUsersByTeamId(team_id) {
+  return db("teams")
+    .join("team_members", "teams.id", "team_members.team_id")
+    .join("users", "users.id", "team_members.user_id")
+    .where("team_members.team_id", team_id)
+    .select("teams.name as team_name", "users.id as user_id", "users.first_name", "users.last_name")
 }
--- teams/teamRouter.js
+++ teams/teamRouter.js
@@ -1,23 +1,57 @@
-const express = require('express');
+const express = require("express");
 
-const Teams = require('../teams/teamModel.js');
+const Teams = require("../teams/teamModel.js");
 
 const router = express.Router();
 
 const { validateTeamId } = require("../middleware/middleware");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Teams.find()
-  .then(teams => res.status(200).json(teams))
-  .catch(err => res.status(500).json({ message: "Could not get teams.", error: err }))
+    .then(teams => res.status(200).json(teams))
+    .catch(err => res.status(500).json({ message: "Could not get teams.", error: err }))
 })
 
-router.get('/:id', validateTeamId, (req, res) => {
+router.get("/:id", validateTeamId, (req, res) => {
   const { id } = req.params
 
   Teams.findById(id)
-  .then(team => res.status(200).json(team))
-  .catch(err => res.status(500).json({ message: "Could not get team.", error: err }))
+    .then(team => res.status(200).json(team))
+    .catch(err => res.status(500).json({ message: "Could not get team.", error: err }))
+})
+
+// Get users in a team
+router.get("/:id/users", validateTeamId, (req, res) => {
+  const {id} = req.params
+
+  Teams.getUsersByTeamId(id)
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({message: "Could not get users for this team", error: err}))
+})
+
+// Add a user to a team
+router.post("/:id", validateTeamId, (req, res) => {
+  const { body } = req
+
+  Teams.insertUser(body)
+    .then(count => res.status(201).json(count))
+    .catch(err => res.status(500).json({ message: "Could not add user to team", error: err }))
+})
+
+// Update team info
+router.put("/:id", validateTeamId, (req, res) => {
+  const updates = { ...req.body, updated_at: new Date(Date.now()).toISOString() }
+  const { id } = req.params
+
+  Teams.update(id, updates)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count)
+      } else {
+        res.status(404).json({ message: 'That team id is not available for update.' })
+      }
+    })
+    .catch(err => res.status(500).json({ message: "Could not update team information", error: err }))
 })
 
 module.exports = router;
