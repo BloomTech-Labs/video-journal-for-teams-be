2b4ef787eb83227c0fc4d89d1effe2e1702591b3
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2020-02-03 10:57:23 -0800

    Updated userRouter tests to support authenticated routes.

--- users/userRouter.spec.js
+++ users/userRouter.spec.js
@@ -16,73 +16,107 @@ afterAll(async () => {
 
 describe("User Data Routes", () => {
   describe("GET /users", () => {
-    it("should return status code 200", async () => {
+    it("should return status code 401 without a token", async () => {
       const response = await request(server)
       .get("/api/users")
       
-      expect(response.status).toEqual(200);
-    });
-
-    it("should return an array", async () => {
-      const response = await request(server)
-        .get("/api/users")
-      
-      expect(Array.isArray(response.body)).toBe(true);
+      expect(response.status).toEqual(401);
     });
   })
+  
+  describe("Authenticated routes", () => {
+    let token;
 
-  describe("GET /users/:id", () => {
-    it("should return status code 200", async () => {
-      const response = await request(server)
-      .get("/api/users/1")
-      
-      expect(response.status).toEqual(200);
+    beforeAll(() => {
+      return request(server)
+      .post("/api/auth/login/username")
+      .send({
+        username: "fwilloughley0",
+        password: "4OTUUVDkYT",
+      })
+      .then(res => {
+        token = res.body.token
+      })
     });
 
-    it("should return status code 400 if the user id is invalid", async () => {
-      const response = await request(server)
-      .get("/api/users/150")
-      
-      expect(response.status).toEqual(400);
+    describe("GET /users/", () => {
+
+      it("should return status code 200", async () => {
+
+        const response = await request(server)
+        .get("/api/users")
+        .set("authorization", token)
+        
+        expect(response.status).toEqual(200);
+      });
+
+      it("should return an array", async () => {
+        const response = await request(server)
+          .get("/api/users")
+          .set("authorization", token)
+        
+        expect(Array.isArray(response.body)).toBe(true);
+      });
     })
 
-    it("should return a properly formatted JSON object on success", async () => {
-      const response = await request(server)
+    describe("GET /users/:id", () => {
+      it("should return status code 200", async () => {
+        const response = await request(server)
         .get("/api/users/1")
-      
-        expect(response.body).toEqual(
-          expect.objectContaining({
-            id: expect.any(Number),
-            email: expect.any(String),
-            username: expect.any(String),
-            password: expect.any(String),
-            first_name: expect.any(String),
-            last_name: expect.any(String)
-          })
-        );
-    });
-    
-  })
+        .set("authorization", token)
+        
+        expect(response.status).toEqual(200);
+      });
 
-  describe("GET /users/:id/teams", () => {
-    it("should return status code 200", async () => {
-      const response = await request(server)
-      .get("/api/users/1/teams")
-      
-      expect(response.status).toEqual(200);
-    });
+      it("should return status code 400 if the user id is invalid", async () => {
+        const response = await request(server)
+        .get("/api/users/150")
+        .set("authorization", token)
+        
+        expect(response.status).toEqual(400);
+      })
 
-    it("should return an array", async () => {
-      const response = await request(server)
+      it("should return a properly formatted JSON object on success", async () => {
+        const response = await request(server)
+          .get("/api/users/1")
+          .set("authorization", token)
+        
+          expect(response.body).toEqual(
+            expect.objectContaining({
+              id: expect.any(Number),
+              email: expect.any(String),
+              username: expect.any(String),
+              password: expect.any(String),
+              first_name: expect.any(String),
+              last_name: expect.any(String)
+            })
+          );
+      });
+    })
+
+    describe("GET /users/:id/teams", () => {
+      it("should return status code 200", async () => {
+        const response = await request(server)
         .get("/api/users/1/teams")
-      
-      expect(Array.isArray(response.body)).toBe(true);
-    });
-    it("should return status code 400 if the user id is invalid", async () => {
-      const response = await request(server)
-      .get("/api/users/150/teams")
-      
-      expect(response.status).toEqual(400);
+        .set("authorization", token)
+        
+        expect(response.status).toEqual(200);
+      });
+
+      it("should return an array", async () => {
+        const response = await request(server)
+          .get("/api/users/1/teams")
+          .set("authorization", token)
+        
+        expect(Array.isArray(response.body)).toBe(true);
+      });
+      it("should return status code 400 if the user id is invalid", async () => {
+        const response = await request(server)
+        .get("/api/users/150/teams")
+        .set("authorization", token)
+        
+        expect(response.status).toEqual(400);
+      })
     })
   })
 })
