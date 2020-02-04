3b99b7c048903b75f56ac34a06eb34ba5f61f08e
Kerri-Ann <kaybee1jam@yahoo.com>
2020-02-03 20:03:54 -0500

    completed tests for PUT requests

--- teams/teamRouter.spec.js
+++ teams/teamRouter.spec.js
@@ -19,8 +19,8 @@ describe("Team Data Routes", () => {
   describe("GET /teams", () => {
     it("should return status code 401 without a token", async () => {
       const response = await request(server)
-      .get("/api/teams")
-      
+        .get("/api/teams")
+
       expect(response.status).toEqual(401);
     });
   })
@@ -30,30 +30,30 @@ describe("Team Data Routes", () => {
 
     beforeAll(() => {
       return request(server)
-      .post("/api/auth/login/username")
-      .send({
-        username: "fwilloughley0",
-        password: "4OTUUVDkYT",
-      })
-      .then(res => {
-        token = res.body.token
-      })
+        .post("/api/auth/login/username")
+        .send({
+          username: "fwilloughley0",
+          password: "4OTUUVDkYT",
+        })
+        .then(res => {
+          token = res.body.token
+        })
     });
 
     describe("GET /teams", () => {
       it("should return status code 200", async () => {
         const response = await request(server)
-        .get("/api/teams")
-        .set("authorization", token)
-        
+          .get("/api/teams")
+          .set("authorization", token)
+
         expect(response.status).toEqual(200);
       });
-    
+
       it("should return an array", async () => {
         const response = await request(server)
           .get("/api/teams")
           .set("authorization", token)
-        
+
         expect(Array.isArray(response.body)).toBe(true);
       });
     })
@@ -61,38 +61,40 @@ describe("Team Data Routes", () => {
     describe("GET /teams/:id", () => {
       it("should return status code 200", async () => {
         const response = await request(server)
-        .get("/api/teams/1")
-        .set("authorization", token)
-        
+          .get("/api/teams/1")
+          .set("authorization", token)
+
         expect(response.status).toEqual(200);
       });
 
       it("should return status code 400 if the team id is invalid", async () => {
         const response = await request(server)
-        .get("/api/teams/150")
-        .set("authorization", token)
-        
+          .get("/api/teams/150")
+          .set("authorization", token)
+
         expect(response.status).toEqual(400);
       })
-    
+
       it("should return a properly formatted JSON object on success", async () => {
         const response = await request(server)
           .get("/api/teams/1")
           .set("authorization", token)
-        
-          expect(response.body).toEqual(
-            expect.objectContaining({
-              id: expect.any(Number),
-              name: expect.any(String),
-              description: expect.any(String),
-              created_at: expect.any(String),
-              updated_at: expect.any(String)
-            })
-          )}
+
+        expect(response.body).toEqual(
+          expect.objectContaining({
+            id: expect.any(Number),
+            name: expect.any(String),
+            description: expect.any(String),
+            created_at: expect.any(String),
+            updated_at: expect.any(String)
+          })
         )
-      })
+      }
+      )
+    })
 
     describe("GET /teams/:id/users", () => {
+
       it("should return status code 200", async () => {
         const response = await request(server)
           .get("/api/teams/1/users")
@@ -107,7 +109,7 @@ describe("Team Data Routes", () => {
           .set("authorization", token)
 
         expect(response.status).toEqual(400);
-      })
+      });
 
       it("should return array of data", async () => {
         const response = await request(server)
@@ -115,10 +117,11 @@ describe("Team Data Routes", () => {
           .set("authorization", token)
 
         expect(Array.isArray(response.body)).toBe(true);
-      })
-    })
+      });
+    });
 
     describe("POST /teams/:id", () => {
+
       it("should return status code 201", async () => {
         const userToAdd = {
           "team_id": 1,
@@ -133,7 +136,7 @@ describe("Team Data Routes", () => {
 
         expect(response.status).toEqual(201);
       });
-      
+
       it("should return status code 400 for missing data", async () => {
         const userToAdd = {
           "team_id": 1,
@@ -147,10 +150,39 @@ describe("Team Data Routes", () => {
 
         expect(response.status).toEqual(400);
       });
+    });
 
-    })    
+    describe("PUT /teams/:id", () => {
 
-  })
-})
+      it("should return status code 200", async () => {
+        const changes = {
+          "name": "Team Iron Man",
+          "description": "The ultimate avengers team"
+        }
 
+        const response = await request(server)
+          .put("/api/teams/1")
+          .set("authorization", token)
+          .send(changes);
 
+        expect(response.status).toEqual(200);
+
+      });
+
+      it("should return status code 400 for wrong id", async () => {
+        const changes = {
+          "name": "Team Iron Man",
+          "description": "The ultimate avengers team"
+        }
+
+        const response = await request(server)
+          .put("/api/teams/111")
+          .set("authorization", token)
+          .send(changes);
+
+        expect(response.status).toEqual(400);
+
+      });
+    });
+  });
+});
