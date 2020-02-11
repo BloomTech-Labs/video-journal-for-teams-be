5f65b4762fbedb725bb908c7cc404dde6883a3ba
Michael Nunes <tinysquid.git@gmail.com>
2020-02-11 13:59:03 -0800

    Update team router to reduce duplicate db actions

--- middleware/validateTeamId.js
+++ middleware/validateTeamId.js
@@ -1,16 +1,16 @@
 const Teams = require("../teams/teamModel.js");
 
 module.exports = function validateTeamId(req, res, next) {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  Teams.findById(id)
-    .then((team) => {
-      if (team) {
-        req.team = team;
-        next();
-      } else {
-        res.status(400).json({ message: "Invalid team id." });
-      }
-    })
-    .catch((err) => res.status(500).json({ message: "'Failed to get team from database'", error: err }));
+	Teams.findById(id)
+		.then((team) => {
+			if (team) {
+				req.team = team;
+				next();
+			} else {
+				res.status(404).json({ message: "Team not found." });
+			}
+		})
+		.catch((err) => res.status(500).json({ message: "Failed to get team from database.", error: err }));
 };
