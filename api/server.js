a3f53261ad80a6afa346cc4a07ba2665939c1e20
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-02-18 17:51:34 -0800

    added team lead privilege on invite link post

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ server.use("/api/auth", AuthRouter);
 server.use("/api/users", passport.authenticate("jwt", { session: false }), UserRouter);
 server.use("/api/teams", passport.authenticate("jwt", { session: false }), TeamRouter);
 server.use("/api/videos", passport.authenticate("jwt", { session: false }), VideoRouter);
-server.use("/api/invites", InviteRouter);
+server.use("/api/invites", passport.authenticate("jwt", { session: false }), InviteRouter);
 server.use("/api/avatars", AvatarRouter);
 
 server.use("/public", express.static(path.join(__dirname, "../public")));
@@ -33,10 +33,10 @@ server.use("/public", express.static(path.join(__dirname, "../public")));
 server.use(function (err, req, res, next) {
 	console.log('This is the invalid field ->', err.field)
 	next(err)
-  })
+})
 
 server.get("/", (req, res) => {
-  res.status(200).json({ api: "running" });
+	res.status(200).json({ api: "running" });
 });
 
 module.exports = server;
--- invites/inviteRouter.js
+++ invites/inviteRouter.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const Invites = require("./inviteModel.js");
 const greek = require("./greekalpha.json");
-const Teams = require("../teams/teamModel.js");
+const { validateTeamId, validateMembership } = require("../middleware/middleware");
+const { isTeamLead } = require("../utils/utils");
 
 const router = express.Router();
 
@@ -38,7 +39,7 @@ router.get("/:code", (req, res) => {
 })
 
 // 2. Returns invite object
-router.post("/", (req, res) => {
+router.post("/:id", validateTeamId, validateMembership, (req, res) => {
 
 	// #region docstring 
 	/*
@@ -81,88 +82,91 @@ router.post("/", (req, res) => {
 		* Creates a single word: McClure-ThetaPhiTau
 	*/
 	// #endregion 
+	const team_id = req.params.id;
+	const { team_name } = req.body;
 
-	const { team_id, team_name } = req.body;
+	if (!isTeamLead(req.user.role)) {
+		res.status(403).json({ message: "Permission denied." });
+	} else {
+		if (!team_id || !team_name) {
+			res.status(400).json({ message: "Request needs to be an object with team_id and team_name elements.", body: req.body })
+		}
 
-	if (!team_id || !team_name) {
-		res.status(400).json({ message: "Request needs to be an object with team_id and team_name elements.", body: req.body  })
-	}
+		// generate a code ahead of time, and create the db object.
+		const newcode = genCode(team_name);
 
-	// generate a code ahead of time, and create the db object.
-	const newcode = genCode(team_name);
+		const dbsend = { team_id, newcode: newcode }
 
-	const dbsend = { team_id, newcode: newcode }
+		Invites.findByTeam(team_id)
+			.then(invite => {
+				const expires = Date.parse(invite.expires_at)
 
-	Invites.findByTeam(team_id)
-		.then(invite => {
-			const expires = Date.parse(invite.expires_at)
-
-			if (expires > Date.now() && invite.isValid === true) {
+				if (expires > Date.now() && invite.isValid === true) {
+					/* 
+						THIS FOUND A VIABLE CODE
+						It will do nothing but return the existing code.
+					 */
+					res.status(200).json({ message: "Existing, valid invite code", ...invite })
+				}
+				if (expires < Date.now() || invite.isValid === false) {
+					/* 
+						THIS FOUND A NON-VIABLE CODE
+						It will use the generated code to
+						UPDATE EXISTING in the db and return the code.
+					 */
+					clg("A CODE EXISTS AND IS EITHER EXPIRED or !isValid")
+					Invites.update(dbsend)
+						.then(updated => {
+							res.status(200).json({ message: "Update of code (expired or invalid) successful", ...updated })
+						})
+						.catch(err => {
+							res.status(500).json({
+								message: `Update existing invite ${invite} error.`,
+								error: `${err}`
+							})
+						})
+				}
+			})
+			.catch(err => {
 				/* 
-					THIS FOUND A VIABLE CODE
-					It will do nothing but return the existing code.
-				 */
-				res.status(200).json({ message: "Existing, valid invite code", ...invite })
-			}
-			if (expires < Date.now() || invite.isValid === false) {
-				/* 
-					THIS FOUND A NON-VIABLE CODE
+					THIS CATCH IS A FAILURE TO FIND AN EXISTING, VIABLE CODE.
 					It will use the generated code to
-					UPDATE EXISTING in the db and return the code.
+					INSERT NEW in the db then return the code.
 				 */
-				clg("A CODE EXISTS AND IS EITHER EXPIRED or !isValid")
-				Invites.update(dbsend)
-					.then(updated => {
-						res.status(200).json({ message: "Update of code (expired or invalid) successful", ...updated })
+				Invites.insert(dbsend)
+					.then(inserted => {
+						res.status(200).json({ message: "Creation successful", ...inserted })
 					})
 					.catch(err => {
-						res.status(500).json({
-							message: `Update existing invite ${invite} error.`,
-							error: `${err}`
-						})
+						// clg(113, err.detail)
+						if (err.detail.includes("not present in table \"teams\"")) {
+							res.status(400).json({ message: `Team ${team_id} doesn't exist.`, error: err.detail })
+						} else {
+							res.status(500).json({
+								message: `Error inserting new invite code for team ${team_id}.`,
+								error: err
+							})
+						}
 					})
-			}
-		})
-		.catch(err => {
-			/* 
-				THIS CATCH IS A FAILURE TO FIND AN EXISTING, VIABLE CODE.
-				It will use the generated code to
-				INSERT NEW in the db then return the code.
-			 */
-			Invites.insert(dbsend)
-				.then(inserted => {
-					res.status(200).json({ message: "Creation successful", ...inserted })
-				})
-				.catch(err => {
-					// clg(113, err.detail)
-					if (err.detail.includes("not present in table \"teams\"")) {
-						res.status(400).json({ message: `Team ${team_id} doesn't exist.`, error: err.detail })
-					} else {
-						res.status(500).json({
-							message: `Error inserting new invite code for team ${team_id}.`,
-							error: err
-						})
-					}
-				})
 
-			// clg(120, "Bottom of Invites/POST: \nThat team invitation doesn't exist. One will be made and sent.")
-		})
+				// clg(120, "Bottom of Invites/POST: \nThat team invitation doesn't exist. One will be made and sent.")
+			})
 
 
-	function genCode(team_name) {
-		// generate a new code using the first part of name and 3 greek characters
-		let cull = team_name.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
-		firstword = cull.split(" ")[0];
-		const newcode = greek[rand(greek.length)] + greek[rand(greek.length)] + greek[rand(greek.length)];
-		return `${firstword}-${newcode}`;
+		function genCode(team_name) {
+			// generate a new code using the first part of name and 3 greek characters
+			let cull = team_name.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+			firstword = cull.split(" ")[0];
+			const newcode = greek[rand(greek.length)] + greek[rand(greek.length)] + greek[rand(greek.length)];
+			return `${firstword}-${newcode}`;
+		}
+
+		function rand(max) {
+			// generate a random number from 0 to "max" argument
+			return Math.floor(Math.random() * Math.floor(max));
+		}
 	}
-
-	function rand(max) {
-		// generate a random number from 0 to "max" argument
-		return Math.floor(Math.random() * Math.floor(max));
-	}
-
-})
+});
 
 module.exports = router;
 
