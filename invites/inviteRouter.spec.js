bbe29382f9d45df907fb271dbbca052f27f39967
allensam88 <54912919+allensam88@users.noreply.github.com>
2020-02-18 18:09:20 -0800

    updated invite tests for team leader protected route

--- invites/inviteRouter.spec.js
+++ invites/inviteRouter.spec.js
@@ -15,32 +15,53 @@ afterAll(async () => {
 });
 
 describe("Team Invitation Routes", () => {
-	describe("GET /invitation/:code", () => {
-		it("without a :code should return status code 404", async () => {
-			const response = await request(server)
-				.get("/api/invites")
-			expect(response.status).toEqual(404);
+	describe("Authenticated routes", () => {
+		let token;
+
+		// log in as Curr Ladley to get a token for all tests
+		beforeAll(() => {
+			return request(server)
+				.post("/api/auth/login/username")
+				.send({
+					username: "fwilloughley0",
+					password: "4OTUUVDkYT",
+				})
+				.then((res) => {
+					token = res.body.token;
+				});
 		});
 
-		it("POST should RETURN an object", async () => {
-			const response = await request(server)
-				.post("/api/invites/")
-				.send({ "team_id": 16, "team_name": "Some Temp Name" })
-			expect(typeof response.body).toBe("object");
-		});
+		describe("GET /invitation/:code", () => {
+			it("without a :code should return status code 404", async () => {
+				const response = await request(server)
+					.get("/api/invites")
+					.set("authorization", token);
+				expect(response.status).toEqual(404);
+			});
 
-		it("POST should not accept an invalid team_id", async () => {
-			const response = await request(server)
-				.post("/api/invites/")
-				.send({ "team_id": 999999, "team_name": "Some Temp Name" })
-			expect(response.status).toEqual(400);
-		});
+			it("POST should RETURN an object", async () => {
+				const response = await request(server)
+					.post("/api/invites/20")
+					.send({ "team_name": "Borer, Nienow and Kunde" })
+					.set("authorization", token);
+				expect(typeof response.body).toBe("object");
+			});
 
-		it("POST should not accept an incomplete object", async () => {
-			const response = await request(server)
-				.post("/api/invites/")
-				.send({ "team_id": 4 })
-			expect(response.status).toEqual(400);
-		});
+			it("POST should not accept an incomplete object", async () => {
+				const response = await request(server)
+					.post("/api/invites/20")
+					.send({ "team_id": 4 })
+					.set("authorization", token);
+				expect(response.status).toEqual(400);
+			});
+
+			it("POST should not accept a regular team member, must be team lead only", async () => {
+				const response = await request(server)
+					.post("/api/invites/2")
+					.send({ "team_name": "Huels and Sons" })
+					.set("authorization", token);
+				expect(response.status).toEqual(403);
+			});
+		})
 	})
 })
