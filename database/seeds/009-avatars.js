f89eef35323ba1aa428ecb0e5f367984142b5116
Sofia Levin <sofiafontes@me.com>
2020-02-28 14:02:40 -0800

    Updated avatar seed to use s3 avatars folder.
    Updated avatar seed.

--- database/seeds/009-avatars.js
+++ database/seeds/009-avatars.js
@@ -1,16 +1,37 @@
 const fs = require("fs");
 const path = require("path");
 
-exports.seed = function(knex) {
-  //Get path to avatars
-  const avatarDir = path.join(__dirname, "../../public/avatars");
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+	secretAccessKey: process.env.AWS_SECRET_ACCESS
+});
+
+const s3 = new AWS.S3();
+
+const params = {
+  Bucket: process.env.AWS_S3_BUCKET, /* required */
+  Delimiter: '/',
+  Prefix: 'avatars/'  // Can be your folder name
+}
+
+exports.seed = async function(knex) {
   const avatars = [];
+  let promise = new Promise((resolve, reject) => {
+    s3.listObjectsV2(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+      } else {
+        data.Contents.forEach((avatar) => {
+          avatars.push({ src: `${avatar.Key}` });
+        })
+      }
+      resolve()
+    });
+  })
 
-  //Get every image in the avatar folder and add it to an array
-  fs.readdirSync(avatarDir).forEach((avatar) => {
-    avatars.push({ src: `avatars/${avatar}` });
-  });
-
+  await promise;
   //Insert avatars into db
   return knex("avatars").insert(avatars);
 };
