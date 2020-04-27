b93487435fd7b6d699a18ec3c42b5e1716075b2a
Chris Oakes <chris.oakes@outlook.com>
2020-04-27 16:23:29 -0700

    email fix

--- email/emailRouter.js
+++ email/emailRouter.js
@@ -7,30 +7,21 @@ const router = express.Router();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// const msg = {
-//   to: ['oakes680@gmail.com', 'chris.oakes@outlook.com'],
-//   from: 'test@example.com',
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// };
 
-router.post("/", (req, res) => {
-   
+router.post("/", (req, res) => { 
     
     Users.findByIdEmail(req.body.id)
     .then((item) => {
-    
+
       sgMail.sendMultiple({
         to: [item.email],
-        from: 'alpacavideojournal@gmail.com',
-        templateId: 'f7245d4e-10d2-447c-b134-3913228ecf4a',
+        from: 'alpacavideojournal+123@gmail.com',
+        templateId: '49c28261-ea82-415f-a224-dfe578e7acda',
         subject: 'Someone commented on your video',
         substitutions: {
           comment: req.body.post     
         },
-        
-        // html: `<strong> ${req.body.post}</strong>`,
+    
       })
       .then((email) => res.status(200).json('hi'))
 		  .catch((err) => res.status(500).json({ message: "Could not send.", error: err }));
@@ -44,18 +35,19 @@ router.post("/teams", (req,res) => {
  
    Teams.getUsersByTeamId(req.body.teamId)
    .then((item) => {
+
      let teamEmails = item.map(item => item.email) 
      let teamNames = item.map(item => item.team_name) 
     sgMail.sendMultiple({
       to: teamEmails,
-      from: 'alpacavideojournal@gmail.com',
-      templateId: '525b29ad-6129-4b54-a0de-c93ef82029db',
+      from: 'alpacavideojournal+123@gmail.com',
+      templateId: '08f7389d-f78a-4e7b-a2e3-346793097f27',
       subject: 'New Prompt Has Been Added!',
       substitutions: {
         prompt: req.body.post,
         teamName:  teamNames[0]   
       },
-      //html: `<strong> ${req.body.post} </strong>`,
+  
     })
     .then((email) => res.status(200).json(console.log('this is good')))
     .catch((err) => res.status(500).json(console.log('this is bad') ));
