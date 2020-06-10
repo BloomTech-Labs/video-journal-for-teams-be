7cac847bef0b830f977918939bc6c1b37a46d491
Chris Giroux <chrisgiroux@MacBook-Pro.fios-router.home>
2020-06-10 12:10:03 -0400

    backend staging for merge

--- organization/organizationRouterV2.js
+++ organization/organizationRouterV2.js
@@ -13,6 +13,7 @@ const Teams = require("../teams/teamModel");
 router.post("/", (req, res) => {
   console.log(req.body);
   const org = req.body.name;
+  console.log(org)
   const uid = req.body.uid;
   console.log(org);
   Organizations.insert(org)
