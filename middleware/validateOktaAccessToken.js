cbc94ff4fd565bb72cb1efa84d9b1d740cf28661
Chris Giroux <chrisgiroux@MacBook-Pro.fios-router.home>
2020-06-13 11:27:49 -0400

    using query string to try and fix issue with urlencoded form params

--- middleware/validateOktaAccessToken.js
+++ middleware/validateOktaAccessToken.js
@@ -1,14 +1,18 @@
 const axios = require("axios");
+const qs = require("querystring");
 require("dotenv").config();
 
 const validateOktaAccessToken = (req, res, next) => {
   const token = req.headers.authorization;
-  const introspectUrl = `https://dev-292346.okta.com/oauth2/default/v1/introspect?client_id=${process.env.CLIENT_ID}&token=${token}&token_type_hint=access_token`.replace(
-    /%0A/
-  );
+  const introspectUrl = `https://dev-292346.okta.com/oauth2/default/v1/introspect`;
   axios
     .post(
-      `https://dev-292346.okta.com/oauth2/default/v1/introspect?client_id=0oacbrrfntl0SndJM4x6&token=${token}&token_type_hint=access_token`
+      introspectUrl,
+      qs.stringify({
+        client_id: `${process.env.CLIENT_ID}`,
+        token: `${token}`,
+        token_type_hint: "access_token",
+      })
     )
     .then((response) => {
       console.log("response", response.data);
